c97fe61aa63679bb75999e592a53933bc57122d0
Mirzaazmath <azmath9603399270@gmail.com>
2024-04-15 16:21:32 +0530

    completed UI

--- lib/presentation/start_screen.dart
+++ lib/presentation/start_screen.dart
@@ -1,5 +1,7 @@
+import 'package:bmw_car/utils/show_up_animation.dart';
 import 'package:bmw_car/utils/text_utlis.dart';
 import 'package:bmw_car/widgets/app_bar_widget.dart';
+import 'package:bmw_car/widgets/featutre_widget.dart';
 
 import 'package:flutter/material.dart';
 
@@ -17,31 +19,79 @@ class StartScreen extends StatelessWidget {
           image: DecorationImage(
               image: AssetImage("assets/side_car.jpeg"), fit: BoxFit.fill),
         ),
-        child: const Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
+        child:  Padding(
+          padding:const EdgeInsets.all(20),
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              Hero(
-                tag: "Hello",
-                child: Material(
-                  type: MaterialType.transparency,
-                  child: TextUtil(
-                    text: "Hello Dev_73arner!",
-                    size: 22,
-                    weight: true,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Hero(
-                  tag: "Model",
-                  child: Material(
+              const  Column(
+                children: [
+                  Hero(
+                    tag: "Hello",
+                    child: Material(
                       type: MaterialType.transparency,
-                      child: TextUtil(text: "8 Series Gran Coupe"))),
+                      child: TextUtil(
+                        text: "Hello Dev_73arner!",
+                        size: 22,
+                        weight: true,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Hero(
+                      tag: "Model",
+                      child: Material(
+                          type: MaterialType.transparency,
+                          child: TextUtil(text: "8 Series Gran Coupe"))),
+                ],
+              ),
+              Positioned(
+                left: 0,
+                  child: ShowUpAnimation(
+                    delay: 100,
+                    child: Column(
+                                    children: [
+                    const  Icon(Icons.keyboard_arrow_up_sharp,color: Colors.white,),
+                    const   SizedBox(height: 10,),
+                    CircleAvatar(
+                      backgroundColor: Colors.grey.shade900,
+                      child:const  Icon(Icons.electric_bolt,color: Colors.white,),
+                    ),
+                    const   SizedBox(height: 10,),
+                    Container(
+                      height: 220,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color:  Colors.grey.shade900.withOpacity(0.8),
+                      ),
+                      padding:const  EdgeInsets.all(10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                         const  TextUtil(text: "Last Ride",size: 14,weight: true,color: Colors.lightBlueAccent,),
+                         const  SizedBox(height: 20,),
+                          Image.asset("assets/route.png",color: Colors.white,),
+                         const  FeatureWidget(
+                              title: "100",
+                              icon: Icons.car_rental_outlined,
+                              subTitle: "Mileage"),
+                        ],
+                      ),
+                    ),
+                    const   SizedBox(height: 10,),
+                    CircleAvatar(
+                      backgroundColor: Colors.grey.shade900,
+                      child:const  Icon(Icons.device_thermostat,color: Colors.white,),
+                    ),
+                    const   SizedBox(height: 10,),
+                    const  Icon(Icons.keyboard_arrow_down_sharp,color: Colors.white,),
 
-
+                                    ],
+                                  ),
+                  ))
             ],
           ),
         ),
